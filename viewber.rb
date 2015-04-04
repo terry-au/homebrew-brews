b419d9a921707ab1e480bdfcf4e413696178536f
Kevin Driver <driverkevint@gmail.com>
2015-04-04 02:17:40 -0500

    viewber cask

---
+++ viewber.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'viewber' do
+  version '1.1'
+  sha256 'b1ada29b91def00fa894b074d16a248f58bae60dcf729a9441a5033c1cea0ecd'
+
+  url 'http://simpleauthority.com/viewber/ViewBer%201.1.dmg'
+  name 'ViewBer'
+  homepage 'http://simpleauthority.com/viewber.html'
+  license :unknown
+
+  app 'ViewBer.app'
+end
