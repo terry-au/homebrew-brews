a75dfb8edadf391a77e7c65eeb486176f5508ea7
Kevin Driver <driverkevint@gmail.com>
2015-12-29 02:39:19 -0600

    geo tagging app

--- Casks/geotagphotospro.rb
+++ Casks/geotagphotospro.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'geotagphotospro' do
   version '1.0'
-  sha256 'a1f17bc8f7fc8df1846510218d24dcdec134f3eb859b072d6f7d72a0d3b6bb86'
+  sha256 '7f6cba30fd0c2bc37a6ad8b41cf68b9d7d6b7e0a1ad32673f7cb2a1a17e93811'
 
   url 'http://www.geotagphotos.net/assets/download/Geotag%20Photos%20Pro-macos64-offline.dmg'
   name 'Geotag Photos Pro'
