9a9e41ee16c7672aa3948417e73c801e932432d3
Kevin Driver <driverkevint@gmail.com>
2016-03-03 03:56:03 -0600

    initial commit of scene builder

---
+++ javafx-scene-builder.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class JavaFX-Scene-Builder < Formula
+  homepage 'http://gluonhq.com/open-source/scene-builder/'
+  url 'http://download.gluonhq.com/scenebuilder/8.1.1/SceneBuilder-8.1.1.jar'
+  sha1 '40abbd9f46cb538fe645ea9d62fa210893676a62'
+
+  def script target; <<-EOS.undent
+    #!/bin/sh
+    
+    exec java -jar #{libexec}/SceneBuilder-#{version}.jar
+    EOS
+  end
+
+  def install
+    libexec.install "SceneBuilder-#{version}.jar"
+
+    bin.mkpath
+
+    ['SceneBuilder'].each {|b| (bin+b).write script b} 
+  end
+
+  def test
+    system "true"
+  end
+end
