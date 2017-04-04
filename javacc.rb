538528edc977991f95e0932389220bc6e25b877a
Kevin Driver <driverkevint@gmail.com>
2017-04-04 15:47:03 -0500

    update to 7.0.2 - first try

--- javacc.rb
+++ javacc.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class Javacc < Formula
   homepage 'http://javacc.java.net'
-  url 'https://java.net/projects/javacc/downloads/download/releases/Release%206.1.2/javacc-6.1.2-distribution.zip'
-  sha256 'fa7660fcc24176c566cf5d1c0f9ff4e11f25b9183449b5768101ee923b4db543'
+  url 'https://github.com/javacc/javacc/archive/release_7_0_2.zip'
+  sha256 'ca4b7f4609cc35a1f91e199f23f1a4a84be64c9208dbc986d9a2be4308f4a354'
+  depends_on "ant" => :build
 
   def script target; <<-EOS.undent
     #!/bin/sh
@@ -13,11 +14,13 @@ class Javacc < Formula
   end
 
   def install
-    libexec.install "target/javacc-#{version}.jar"
+    system "ant"
+
+    libexec.install "target/javacc-7.0.2.jar"
 
     bin.mkpath
 
-    ['javacc', 'jjtree', 'jjdoc'].each {|b| (bin+b).write script b} 
+    ['javacc', 'jjtree', 'jjdoc'].each {|b| (bin+b).write script b}
   end
 
   def test
